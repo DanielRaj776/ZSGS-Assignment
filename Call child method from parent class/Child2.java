2c17b518b9f738c2cb50761e2c95eba0b1e49018
DanielRaj776 <112938416+DanielRaj776@users.noreply.github.com>
2022-11-16 12:12:54 +0530

    Delete Call child method from parent class directory

--- Call child method from parent class/Child2.java
+++
@@ -1,9 +0,0 @@
-package parent_child_and_default_method;
-
-class Child2 extends Parent2 {
-        
-    void println() {
-	System.out.println("child's println");
-    }
-    
-}
--- Call child method from parent class/Parent1.java
+++
@@ -1,8 +0,0 @@
-package parent_child_and_default_method;
-
-abstract class Parent1 {
-    void print() {
-	println();
-    }
-    abstract void println();
-}
